ca6bbca187ea5ef16ed64ad29322d6081bb2aa4e
devSiddik <abubokor.diu@gmail.com>
2022-06-26 01:26:44 +0600

    complete home page

--- js/script.js
+++ js/script.js
@@ -15,3 +15,20 @@ $(".nav-item").hover(function() {
 
 
 });
+
+
+
+//---------- NAV BAR FIXED JS ------------//
+$(window).scroll(function(){
+    var couting_scroll = $(window).scrollTop()
+    if(couting_scroll>90){
+        $("#searchBar").addClass("d-none");
+        $("#fixedNavBar").removeClass("d-none");
+        $("#fixedNavBar").addClass("d-block fixed");
+    }
+    else{
+        $("#searchBar").removeClass("d-none");
+        $("#fixedNavBar").addClass("d-none");
+        $("#fixedNavBar").removeClass("d-block fixed");
+    }
+})
